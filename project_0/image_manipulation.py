38239023a6846144c60fece9410f25f1a66c442a
Matthew Diamond <medman826@gmail.com>
2014-09-02 15:04:28 -0500

    Added docstrings

--- project_0/image_manipulation.py
+++ project_0/image_manipulation.py
@@ -14,7 +14,7 @@ import cv2
 
 
 def flip_image(image, horizontal, vertical):
-
+    """Flips the image vertically, horizontally, or both"""
     result = image
     if(horizontal):
         result = cv2.flip(result, 1)
@@ -24,11 +24,11 @@ def flip_image(image, horizontal, vertical):
 
 
 def negate_image(image):
-
+    """Negates the colors in the image"""
     return cv2.bitwise_not(image)
 
 
 def swap_blue_and_green(image):
-
+    """Swaps the B and G color channels in the image"""
     b, g, r = cv2.split(image)
     return cv2.merge((g, b, r))
