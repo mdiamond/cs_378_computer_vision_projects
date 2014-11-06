975b533c57def5572e2c619491d5fcd913c0d5fe
Matthew Diamond <medman826@gmail.com>
2014-11-05 22:52:29 -0600

    Project 3 complete

--- project_3/tracking.py
+++ project_3/tracking.py
@@ -7,6 +7,45 @@ import cv2
 import math
 import numpy
 
+def track_ball(video):
+    """
+    Helper for all track_ball functions
+    """
+    result = []
+
+    while(1):
+        # Take frame from video
+        retval, image = video.read()
+
+        # Did video.read() give us an image?
+        if retval == True:
+
+            # Set up the Region of Interest for the object we want to track
+            hsv_roi = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+            mask = cv2.inRange(hsv_roi,
+                       numpy.array((0., 60., 32.)),
+                       numpy.array((180., 255., 255.)))
+            roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
+
+            # Convert from BGR to HSV color-space
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+            # Calculate the back projection of the histogram of the ball
+            dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+
+            # Applies a fixed-level threshold to each array element
+            ret, thresh = cv2.threshold(dst, 127, 255, 0)
+
+            # contours is a list of all the contours in the image
+            contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            # Gets the XYWH of each of the faces detected and appends them
+            x,y,w,h = cv2.boundingRect(contours[0])
+            result.append((x, y, x + w, y + h))
+
+        else:
+            break
+    cv2.destroyAllWindows()
+    return result
 
 def track_ball_1(video):
     """Track the ball's center in 'video'.
@@ -19,92 +58,53 @@ def track_ball_1(video):
       a list of (min_x, min_y, max_x, max_y) four-tuples containing the pixel
       coordinates of the rectangular bounding box of the ball in each frame.
     """
-    result = []
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    fgbg = cv2.BackgroundSubtractorMOG()
-    if(video.isOpened()):
-        hasFrame = True
-        while(hasFrame):
-            ret, frame = video.read()
-            if(ret != 0):
-                fgmask = fgbg.apply(frame)
-                fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-                imgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-                contours, hierarchy = cv2.findContours(thresh,
-                        cv2.RETR_TREE,
-                        cv2.CHAIN_APPROX_SIMPLE)
-                x, y, w, h = cv2.boundingRect(contours[0])
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                cv2.imshow('frame', frame)
-                result.append((x, y, x + w, y + h))
-                if cv2.waitKey(5) & 0xFF == ord('q'):
-                    break
-            else:
-                hasFrame = False
-    cv2.destroyAllWindows()
-    return result
+    return track_ball(video)
 
 
 def track_ball_2(video):
     """As track_ball_1, but for ball_2.mov."""
-    result = []
-
-    # take first frame of the video
-    ret, frame = video.read()
-    
-    # setup initial location of window
-    r, h, c, w = 65, 65, 65, 65  # simply hardcoded the values
-    track_window = (c, r, w, h)
-    x, y, w, h = track_window
-    result.append((x, y, x + w, y + h))
-
-    # set up the ROI for tracking
-    roi = frame[r:r+h, c:c+w]
-    hsv_roi = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv_roi,
-                       numpy.array((0., 60., 32.)),
-                       numpy.array((180., 255., 255.)))
-    roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
-    cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
-
-    # setup the termination criteria, either 10 iteration or move by atleast 1 pt
-    term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
-    while(1):
-        ret, frame = video.read()
-
-        if ret == True:
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
-
-            # apply meanshift to get the new location
-            ret, track_window = cv2.meanShift(dst, track_window, term_crit)
-
-            # draw it on image
-            x, y, w, h = track_window
-            result.append((x, y, x + w, y + h))
-
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv2.imshow('img2', frame)
-
-            if cv2.waitKey(5) & 0xFF == ord('q'): # Escape key
-                break
-
-        else:
-            break
-    cv2.destroyAllWindows()
-    return result
+    return track_ball(video)
 
 
 def track_ball_3(video):
-    """As track_ball_1, but for ball_2.mov."""
     result = []
+    num_frames = 0
+    fgbg = cv2.BackgroundSubtractorMOG()
+    x, y, w, h = 0, 0, 0, 0
+    avg_w, avg_h = 0, 0
+    while(1):
+        ret, frame = video.read()
+
+        if ret == True:
+            num_frames = num_frames + 1
+            sub = fgbg.apply(frame)
+
+            kernel = numpy.ones((5,5),numpy.uint8)
+            dilation = cv2.dilate(sub,kernel,iterations = 1)
+            ret, thresh = cv2.threshold(dilation, 127, 255, 0)
+
+            # contours is a list of all the contours in the image
+            contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            if (contours):
+                x, y, w, h = cv2.boundingRect(contours[0])
+                avg_w = avg_w + w
+                avg_h = avg_h + h
+
+        else:
+            break
+
+    avg_w = avg_w / num_frames
+    avg_h = avg_h / num_frames
+
+    # Reset the video
+    video.set(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO, 0)
 
     # take first frame of the video
     ret, frame = video.read()
     
     # setup initial location of window
-    r, h, c, w = 65, 65, 65, 65  # simply hardcoded the values
+    r, h, c, w = avg_w, avg_h, avg_w, avg_h  # simply hardcoded the values
     track_window = (c, r, w, h)
     x, y, w, h = track_window
     result.append((x, y, x + w, y + h))
@@ -121,10 +121,11 @@ def track_ball_3(video):
     # setup the termination criteria, either 10 iteration or move by atleast 1 pt
     term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
     while(1):
-        ret, frame = video.read()
+        retval, image = video.read()
 
-        if ret == True:
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        if retval == True:
+
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
             dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
 
             # apply meanshift to get the new location
@@ -133,12 +134,6 @@ def track_ball_3(video):
             # draw it on image
             x, y, w, h = track_window
             result.append((x, y, x + w, y + h))
-
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv2.imshow('img2', frame)
-
-            if cv2.waitKey(5) & 0xFF == ord('q'): # Escape key
-                break
 
         else:
             break
@@ -148,67 +143,32 @@ def track_ball_3(video):
 
 def track_ball_4(video):
     """As track_ball_1, but for ball_2.mov."""
-    result = []
-
-    # take first frame of the video
-    ret, frame = video.read()
-    
-    # setup initial location of window
-    r, h, c, w = 65, 65, 65, 65  # simply hardcoded the values
-    track_window = (c, r, w, h)
-    x, y, w, h = track_window
-    result.append((x, y, x + w, y + h))
-
-    # set up the ROI for tracking
-    roi = frame[r:r+h, c:c+w]
-    hsv_roi = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv_roi,
-                       numpy.array((0., 60., 32.)),
-                       numpy.array((180., 255., 255.)))
-    roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
-    cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
-
-    # setup the termination criteria, either 10 iteration or move by atleast 1 pt
-    term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
-    while(1):
-        ret, frame = video.read()
-
-        if ret == True:
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
-
-            # apply meanshift to get the new location
-            ret, track_window = cv2.meanShift(dst, track_window, term_crit)
-
-            # draw it on image
-            x, y, w, h = track_window
-            result.append((x, y, x + w, y + h))
-
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv2.imshow('img2', frame)
-
-            if cv2.waitKey(5) & 0xFF == ord('q'): # Escape key
-                break
-
-        else:
-            break
-    cv2.destroyAllWindows()
-    return result
+    return track_ball(video)
 
 
 def track_face(video):
     """As track_ball_1, but for face.mov."""
     result = []
+
+    # Detects face in a video stream
     cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+    # Reads the video every frame and tries to detect a face
     if(video.isOpened()):
         hasFrame = True
         while(hasFrame):
             ret, frame = video.read()
             if(ret != 0):
+
+                # Converts stream color to grey
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                # Equalizes the histogram to get a better gray image
                 gray = cv2.equalizeHist(gray)
 
+                # Detects faces of different sizes using cv2.CASCADE_SCALE_IMAGE
+                # Minimum neighbors a candidate needs so it can saved is 6
+                # minSize is (70, 70) to prevent artifacts
                 faces = cascade.detectMultiScale(gray,
                                                  scaleFactor=1.2,
                                                  minNeighbors=6,
@@ -216,17 +176,11 @@ def track_face(video):
                                                  flags =
                                                  cv2.CASCADE_SCALE_IMAGE)
 
+                # Gets the XYWH of each of the faces detected and appends them
                 for (x, y, w, h) in faces:
-                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
                     result.append(((x, y, x + w, y + h)))
-
-                cv2.imshow('Video', frame)
-
-                if (cv2.waitKey(1) & 0xff) == 27:
-                    break
 
             else:
                 hasFrame = False
 
-    cv2.destroyAllWindows()
     return result
